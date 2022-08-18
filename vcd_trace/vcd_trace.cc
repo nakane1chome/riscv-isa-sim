30af59f65c99f2667a6a74c1ccd8e738b5edef47
Phil <pmulholland@shincbm.com>
2022-08-18 23:31:25 +0930

    Add vcd_trace wrapper for cpp_vcd_tracer

---
+++ vcd_trace/vcd_trace.cc
@@ -0,0 +1,14 @@
+#include "cpp_vcd_tracer/src/vcd_tracer.cpp"
+
+#include"softfloat_types.h"
+
+namespace vcd_tracer { 
+    volatile vcd_tracer::scope_fn::sequence_t trace_seq = 0;
+
+    template<> void value_base::dump<float128_t>(std::ostream &out, 
+                                                 const size_t bit_size, 
+                                                 const value_base::value_state state, 
+                                                 const float128_t value) const {
+    }
+}
+
---
+++ vcd_trace/vcd_trace.h
@@ -0,0 +1,36 @@
+#ifndef VCD_TRACE_H
+#define VCD_TRACE_H
+
+#include "cpp_vcd_tracer/src/vcd_tracer.hpp"
+
+#include "softfloat_types.h"
+
+namespace vcd_tracer { 
+
+    static constexpr int PC_TRACE_DEPTH=5000; // Should match sim_t::INTERLEAVE.
+    static constexpr int REG_TRACE_DEPTH=2000; 
+    static constexpr int CSR_TRACE_DEPTH=500; 
+    static constexpr int BUS_TRACE_WIDTH=32; 
+
+    extern volatile vcd_tracer::scope_fn::sequence_t trace_seq;
+
+    template<>
+    inline bool sample_changed(const float128_t new_sample, const float128_t prev_sample) {
+        // TODO
+        return true;
+    }
+    
+
+    template<typename T> using sim_pc_value = value<T, vcd_tracer::bit_size<T>::value, PC_TRACE_DEPTH, &trace_seq>;
+    template<typename T> using sim_reg_value = value<T, vcd_tracer::bit_size<T>::value, REG_TRACE_DEPTH, &trace_seq>;
+    template<typename T> using sim_csr_value = value<T, vcd_tracer::bit_size<T>::value, CSR_TRACE_DEPTH, &trace_seq>;
+
+}
+
+static std::ostream &operator<<(std::ostream &out, const float128_t) {
+    // NOP
+    return out;
+}
+
+#endif
+
